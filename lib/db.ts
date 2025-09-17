c7fcffbd5d503fd8544b986b4ab35c30bd12a9e2
sultonovfazliddin09 <newgithub09@gmail.com>
2025-09-17 17:04:13 +0500

    envv

--- lib/db.ts
+++ lib/db.ts
@@ -5,5 +5,4 @@ declare global {
 }
 
 export const db = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === "development") global.prisma = db;
+if (process.env.NODE_ENV !== "development") global.prisma = db;
